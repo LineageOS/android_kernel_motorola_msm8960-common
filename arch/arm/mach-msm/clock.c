fcd86ac26b3967d31a4d02e76049f82a69d9dbf6
Jean Pihet <j-pihet@ti.com>
2011-08-25 15:35:03 +0200

    PM QoS: Move and rename the implementation files
    The PM QoS implementation files are better named
    kernel/power/qos.c and include/linux/pm_qos.h.
    
    The PM QoS support is compiled under the CONFIG_PM option.
    
    Signed-off-by: Jean Pihet <j-pihet@ti.com>
    Acked-by: markgross <markgross@thegnar.org>
    Reviewed-by: Kevin Hilman <khilman@ti.com>
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>
    
    Conflicts:
    
    	arch/arm/mach-msm/clock.c
    	kernel/Makefile
    
    Change-Id: I7b3a61521b8ba15971ea21182eb89f1090f4fa48

--- arch/arm/mach-msm/clock.c
+++ arch/arm/mach-msm/clock.c
@@ -17,6 +17,7 @@
 #include <linux/kernel.h>
 #include <linux/err.h>
 #include <linux/spinlock.h>
+#include <linux/pm_qos.h>
 #include <linux/string.h>
 #include <linux/module.h>
 #include <linux/clk.h>
