8171a85071f100b745000711afec92b83175906f
Neema Shetty <nshetty@codeaurora.org>
2012-05-09 12:01:39 -0700

    msm: 8064: Add support for Slimbus3 Rx CPU DAI.
    Use Slimbus3 rx CPU DAI to send the mixed audio and voice streams
    from the application processor to the external modem for
    echo-cancellation.
    
    Change-Id: I275ae11c1ec84bd49f482892126ee65dfa7d68fc
    Signed-off-by: Neema Shetty <nshetty@codeaurora.org>
    
    Conflicts:
    
    	arch/arm/mach-msm/board-8064.c

--- arch/arm/mach-msm/board-8064.c
+++ arch/arm/mach-msm/board-8064.c
@@ -758,6 +758,7 @@ static struct platform_device *common_devices[] __initdata = {
 	&apq_cpudai_slimbus_1_rx,
 	&apq_cpudai_slimbus_1_tx,
 	&apq_cpudai_slimbus_2_tx,
+	&apq_cpudai_slimbus_3_rx,
 	&apq8064_device_cache_erp,
 	&msm_pil_vidc,
 	&apq8064_iommu_domain_device,
