c882d6e05946690d5f2809cf5b4e66afad145d37
Kiran Kandi <kkandi@codeaurora.org>
2012-05-21 23:03:26 -0700

    ASoC: msm: Add SLIMBUS_2_RX CPU DAI
    Add support for a playback CPU DAI on SLIMBUS_2_RX AFE port.
    
    Change-Id: Ib79e648cfab09e738d4953a20d895a5bb53ffca2
    Signed-off-by: Kiran Kandi <kkandi@codeaurora.org>

--- sound/soc/msm/qdsp6/q6afe.c
+++ sound/soc/msm/qdsp6/q6afe.c
@@ -151,6 +151,7 @@ int afe_get_port_type(u16 port_id)
 	case HDMI_RX:
 	case SLIMBUS_0_RX:
 	case SLIMBUS_1_RX:
+	case SLIMBUS_2_RX:
 	case SLIMBUS_3_RX:
 	case INT_BT_SCO_RX:
 	case INT_BT_A2DP_RX:
@@ -214,6 +215,7 @@ int afe_validate_port(u16 port_id)
 	case SLIMBUS_0_TX:
 	case SLIMBUS_1_RX:
 	case SLIMBUS_1_TX:
+	case SLIMBUS_2_RX:
 	case SLIMBUS_2_TX:
 	case SLIMBUS_3_RX:
 	case SLIMBUS_3_TX:
@@ -283,6 +285,7 @@ int afe_get_port_index(u16 port_id)
 	case SLIMBUS_0_TX: return IDX_SLIMBUS_0_TX;
 	case SLIMBUS_1_RX: return IDX_SLIMBUS_1_RX;
 	case SLIMBUS_1_TX: return IDX_SLIMBUS_1_TX;
+	case SLIMBUS_2_RX: return IDX_SLIMBUS_2_RX;
 	case SLIMBUS_2_TX: return IDX_SLIMBUS_2_TX;
 	case SLIMBUS_3_RX: return IDX_SLIMBUS_3_RX;
 	case SLIMBUS_3_TX: return IDX_SLIMBUS_3_TX;
@@ -319,6 +322,7 @@ int afe_sizeof_cfg_cmd(u16 port_id)
 	case SLIMBUS_0_TX:
 	case SLIMBUS_1_RX:
 	case SLIMBUS_1_TX:
+	case SLIMBUS_2_RX:
 	case SLIMBUS_2_TX:
 	case SLIMBUS_3_RX:
 	case SLIMBUS_3_TX:
