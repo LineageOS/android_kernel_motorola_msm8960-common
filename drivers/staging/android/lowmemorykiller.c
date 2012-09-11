9876f225d7e30adc170b31d71a047f3bd11b2504
Steve Kondik <shade@chemlab.org>
2012-09-11 18:34:41 +0100

    lowmemorykiller: Compact memory when killing processes
    * Memory compaction is never invoked on Android because we avoid swap
       and don't allocate huge pages. Fix it by invoking compaction from the LMK when
       a process is killed to reduce memory fragmentation.
    
    Conflicts:
    
    	drivers/staging/android/lowmemorykiller.c
    
    Change-Id: I1bcf639fb7016a38f946d640b960e813225dded0

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -40,6 +40,7 @@
 #include <linux/memory.h>
 #include <linux/memory_hotplug.h>
 #include <linux/swap.h>
+#include <linux/compaction.h>
 
 static uint32_t lowmem_debug_level = 1;
 static int lowmem_adj[6] = {
@@ -60,6 +61,8 @@ static int lowmem_minfree_size = 4;
 static unsigned int offlining;
 static struct task_struct *lowmem_deathpending;
 static unsigned long lowmem_deathpending_timeout;
+
+extern int compact_nodes();
 
 #define lowmem_print(level, x...)			\
 	do {						\
@@ -232,6 +235,8 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	lowmem_print(4, "lowmem_shrink %lu, %x, return %d\n",
 		     sc->nr_to_scan, sc->gfp_mask, rem);
 	read_unlock(&tasklist_lock);
+    if (selected)
+        compact_nodes();
 	return rem;
 }
 
