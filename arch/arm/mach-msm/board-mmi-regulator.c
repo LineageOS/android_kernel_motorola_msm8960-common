19ecded656bf8a220deefc9c59e9d72662a24453
nadlabak <pavel@doshaska.net>
2013-08-26 00:12:45 +0200

    Update copyright to The Linux Foundation
    Change-Id: I666b0636a6059417ecf6f762006570d47e9d5a05

--- arch/arm/mach-msm/board-mmi-regulator.c
+++ arch/arm/mach-msm/board-mmi-regulator.c
@@ -3,7 +3,7 @@
  *
  * Derived from board-msm8960-regulator.c.
  *
- * Copyright (c) 2011, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2011, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
